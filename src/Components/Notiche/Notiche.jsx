9d5d81867cda4037bbbd9452727a21fc4842629a
netstermonir <monirhossain6163@gmail.com>
2021-12-19 14:20:08 +0600

    some

--- src/Components/Notiche/Notiche.jsx
+++ src/Components/Notiche/Notiche.jsx
@@ -11,31 +11,25 @@ const Notiche = () => {
                 </Typography>
                 <div className='notich-box'>
                     {/* eslint-disable-next-line jsx-a11y/no-distracting-elements */}
-                    <marquee behavior="scroll" direction="up" scrollamount='3px' onMouseOver='this.stop()' onMouseOut='this.start()'>
+                    <marquee direction="up" scrollamount='4px'>
                         <ul>
                             <li>
                                 <a href="https://mdmonir.netlify.app/">আগামী নির্বাচনী পরীক্ষা ৩১ মার্চের পরিবর্তে ৫ জুন অনুষ্ঠিত হবে</a>
                             </li>
                             <li>
-                                <a href="https://mdmonir.netlify.app/">আগামী নির্বাচনী পরীক্ষা ৩১ মার্চের পরিবর্তে ৫ জুন অনুষ্ঠিত হবে</a>
+                                <a href="https://mdmonir.netlify.app/">আগামী ১২ এপ্রিল বিজ্ঞান বিভাগের পরীক্ষা</a>
                             </li>
                             <li>
-                                <a href="https://mdmonir.netlify.app/">আগামী নির্বাচনী পরীক্ষা ৩১ মার্চের পরিবর্তে ৫ জুন অনুষ্ঠিত হবে</a>
+                                <a href="https://mdmonir.netlify.app/">আগামী ৩১ মার্চের মধ্যে সকল ফি পরিশোধ করতে হবে</a>
                             </li>
                             <li>
-                                <a href="https://mdmonir.netlify.app/">আগামী নির্বাচনী পরীক্ষা ৩১ মার্চের পরিবর্তে ৫ জুন অনুষ্ঠিত হবে</a>
+                                <a href="https://mdmonir.netlify.app/">স্কুলের ভিতর মোবাইল ব্যবহার করা সম্পূর্ণ নিষেধ</a>
                             </li>
                             <li>
-                                <a href="https://mdmonir.netlify.app/">আগামী নির্বাচনী পরীক্ষা ৩১ মার্চের পরিবর্তে ৫ জুন অনুষ্ঠিত হবে</a>
+                                <a href="https://mdmonir.netlify.app/">এই স্কুলের ডিজাইনটি ক্রয় করতে যোগাযোগ করুন : 01747706163</a>
                             </li>
                             <li>
-                                <a href="https://mdmonir.netlify.app/">আগামী নির্বাচনী পরীক্ষা ৩১ মার্চের পরিবর্তে ৫ জুন অনুষ্ঠিত হবে</a>
-                            </li>
-                            <li>
-                                <a href="https://mdmonir.netlify.app/">আগামী নির্বাচনী পরীক্ষা ৩১ মার্চের পরিবর্তে ৫ জুন অনুষ্ঠিত হবে</a>
-                            </li>
-                            <li>
-                                <a href="https://mdmonir.netlify.app/">আগামী নির্বাচনী পরীক্ষা ৩১ মার্চের পরিবর্তে ৫ জুন অনুষ্ঠিত হবে</a>
+                                <a href="https://mdmonir.netlify.app/">আমাদের শিক্ষা প্রতিষ্ঠানে আপনাদের স্বাগতম</a>
                             </li>
                         </ul>
                     </marquee>
